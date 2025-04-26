4cb40c3ef59531912e969f286988161579489c25
Andres Hung <2018118240@ub.edu.bz>
2025-04-26 14:34:17 -0600

    Add net utility package

---
+++ internal/netutils/netutils.go
@@ -0,0 +1,40 @@
+package netutils
+
+import "net"
+
+// GetIPv4Addr returns a IPv4 net.Addr for the local machine based on the network ("tcp", "udp") and port.
+func GetIPv4Addr(network string, port int) net.Addr {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return fallbackAddr(network, port)
+	}
+
+	for _, addr := range addrs {
+		// Check that the address is not a loopback
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			// Only use IPv4 addresses
+			if ip := ipNet.IP.To4(); ip != nil {
+				return makeAddr(network, ip, port)
+			}
+		}
+	}
+
+	return fallbackAddr(network, port)
+}
+
+func fallbackAddr(network string, port int) net.Addr {
+	ip := net.ParseIP("127.0.0.1")
+	return makeAddr(network, ip, port)
+}
+
+func makeAddr(network string, ip net.IP, port int) net.Addr {
+	switch network {
+	case "tcp", "tcp4", "tcp6":
+		return &net.TCPAddr{IP: ip, Port: port}
+	case "udp", "udp4", "udp6":
+		return &net.UDPAddr{IP: ip, Port: port}
+	default:
+		// unknown network, fallback to TCPAddr
+		return &net.TCPAddr{IP: ip, Port: port}
+	}
+}
